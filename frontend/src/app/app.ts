2b42495492fb8ada6df287d1b341760b1b308f44
Nacho <igbaci@gmail.com>
2026-03-14 08:22:53 +0100

    Home component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ThermusApp');
+  protected readonly title = signal('Thermus');
 }
